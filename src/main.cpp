1ef82caac13c0ef166392934723543bfaf17cc49
Svengineering <sven.volkmann@uni-wuppertal.de>
2023-03-07 14:05:55 +0100

    new sketch: three-led traffic light with button

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,81 @@
 #include <Arduino.h>
 
-//  TURN ON LED WITH BUTTON PRESS FOR A CERTAIN TIME
-//
-//  pushing a button will turn on a led for a fixed time, 
-//  repeating the button push (before led has turned out) will extend the lighting time
 
+const int RED = 4;
+const int GREEN = 2;
+const int YELLOW = 3;
+const int BUTTON = 5;
 
-const int BUTTON = 7;
-const int LED = 6;
+const int button_reaction_delay = 2000; //the button push shouldn't cause instant reaction
 
-//how long should the led being turned on?
-const int led_timeout_ms = 5000;
+const int button_dead_time = 10000; //the button won't trigger anything for a certain period after a red cycle just has finished
 
+const int red_time = 10000;
+const int yellow_time = 2000;
+const int yellow_red_time = 1000;
 
 void setup() {
-  pinMode(BUTTON, INPUT);
-  pinMode(LED, OUTPUT);
+    pinMode(RED, OUTPUT);
+    pinMode(GREEN, OUTPUT);
+    pinMode(YELLOW, OUTPUT);
+    pinMode(BUTTON, INPUT);
+
+    //init traffic light with green phase
+    digitalWrite(GREEN, HIGH);
+    digitalWrite(YELLOW, LOW);
+    digitalWrite(RED, LOW);
+
+
 }
 
+//
+// one full cycle for a traffic light turning red, staying red and then turning green
+// begins with yellow phase
+//
+void traffic_light_red_cycle() {
 
-void loop() {
-  unsigned long led_start; //time of button push
+    digitalWrite(RED, LOW);
+    digitalWrite(GREEN, LOW);
+    digitalWrite(YELLOW, HIGH);
+    
+    delay(yellow_time);
 
-  //read button status
-  if(digitalRead(BUTTON) == HIGH) {
-    led_start = millis();
-    //always turn on led after button push
-    digitalWrite(LED, HIGH);
-  }
+    digitalWrite(YELLOW, LOW);
+    digitalWrite(RED, HIGH);
 
-  //Check if led should be turned out:
-  //has (time of the button push + led lighting time) expired?
-  if( (led_start + led_timeout_ms) < millis() ) {
-    digitalWrite(LED, LOW);
-  }
+    delay(red_time - yellow_red_time);
 
-  delay(10);
+    digitalWrite(YELLOW, HIGH);
+    
+    delay(yellow_red_time);
+
+    digitalWrite(RED, LOW);
+    digitalWrite(YELLOW, LOW);
+    digitalWrite(GREEN, HIGH);
 }
 
+void loop(){
 
+    //time until a button push is blocked ( = pushing won't have any reaction)
+    static unsigned long button_push_blocked_time = 0;
+
+    //default behaviour: green on, red off, yellow off
+    ;
+
+    //turn on the red cycle?
+    //check if: 1. button dead time is over
+    //          2. button is pushed
+    if( button_push_blocked_time < millis() && digitalRead(BUTTON) == HIGH ) {
+      
+        //after button push wait a bit...
+        delay(button_reaction_delay);
+
+        //start the red cycle for the traffic light
+        traffic_light_red_cycle();
+
+        //red cycle is over: save time until a button push gets ignored
+        button_push_blocked_time = millis() + button_dead_time;
+    }
+
+
+}
---
+++ src/push_button_turn_on_led.cpp
@@ -0,0 +1,42 @@
+#include <Arduino.h>
+
+//  TURN ON LED WITH BUTTON PRESS FOR A CERTAIN TIME
+//
+//  pushing a button will turn on a led for a fixed time, 
+//  repeating the button push (before led has turned out) will extend the lighting time
+
+/*
+const int BUTTON = 7;
+const int LED = 6;
+
+//how long should the led being turned on?
+const int led_timeout_ms = 5000;
+
+
+void setup() {
+  pinMode(BUTTON, INPUT);
+  pinMode(LED, OUTPUT);
+}
+
+
+void loop() {
+  unsigned long led_start; //time of button push
+
+  //read button status
+  if(digitalRead(BUTTON) == HIGH) {
+    led_start = millis();
+    //always turn on led after button push
+    digitalWrite(LED, HIGH);
+  }
+
+  //Check if led should be turned out:
+  //has (time of the button push + led lighting time) expired?
+  if( (led_start + led_timeout_ms) < millis() ) {
+    digitalWrite(LED, LOW);
+  }
+
+  delay(10);
+}
+
+
+*/
